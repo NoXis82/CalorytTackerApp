8bc6b3a1e74496950346ebb587598d779658ca75
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2025-01-20 21:07:28 +0300

    Final Fixes

--- app/src/main/java/ru/noxis/calorytrackerapp/MainActivity.kt
+++ app/src/main/java/ru/noxis/calorytrackerapp/MainActivity.kt
@@ -7,15 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import ru.noxis.calorytrackerapp.navigation.NavigateRoot
 import ru.noxis.calorytrackerapp.ui.theme.CalorytTackerAppTheme
+import ru.noxis.core.domain.preferences.Preferences
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var preferences: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val shouldShowOnboarding = preferences.loadShouldShowOnboarding()
         setContent {
             CalorytTackerAppTheme {
-                NavigateRoot()
+                NavigateRoot(shouldShowOnboarding)
             }
         }
     }
